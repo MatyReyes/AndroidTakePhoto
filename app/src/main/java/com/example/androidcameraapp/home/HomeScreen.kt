b92b81ba5ac585ac294ba679be41dea1620bfe38
Matias Reyes <matirey1@gmail.com>
2024-04-24 09:54:12 -0400

    first commit

---
+++ app/src/main/java/com/example/androidcameraapp/home/HomeScreen.kt
@@ -0,0 +1,106 @@
+package com.example.androidcameraapp.home
+
+import android.Manifest
+import android.view.ViewGroup
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import java.io.File
+import java.util.concurrent.Executor
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+    val cameraController = remember {
+        LifecycleCameraController(context)
+    }
+
+    val lifecycle = LocalLifecycleOwner.current
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+    LaunchedEffect(Unit) {
+        permissionState.launchPermissionRequest()
+    }
+    Scaffold(modifier = Modifier
+        .fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val executor = ContextCompat.getMainExecutor(context)
+                takePicture(cameraController, executor)
+            }) {
+                Text(text = "Camera")
+            }
+        }
+    ) {
+        if (permissionState.status.isGranted){
+            CameraComposable(modifier= Modifier.padding(it), cameraController,lifecycle)
+        }else{
+            Text(
+                text = "Permiso denegado",
+                modifier = Modifier.padding(it)
+            )
+        }
+    }
+
+}
+
+private fun takePicture(
+    cameraController: LifecycleCameraController,
+    executor: Executor
+){
+    val file = File.createTempFile("imagentest",".jpg")
+    val outputDirectory = ImageCapture.OutputFileOptions.Builder(file).build()
+    cameraController.takePicture(
+        outputDirectory,
+        executor,
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                println(outputFileResults.savedUri)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                println()
+            }
+        }
+    )
+}
+
+
+@Composable
+fun CameraComposable(
+    modifier: Modifier = Modifier,
+    cameraController: LifecycleCameraController,
+    lifecycle: LifecycleOwner
+){
+
+    cameraController.bindToLifecycle(lifecycle)
+    AndroidView(factory = { context ->
+        val previewView = PreviewView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+        previewView.controller = cameraController
+
+        previewView
+    })
+}
